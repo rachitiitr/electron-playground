fbcbe04ba87829d42860eb694a445ac92bb3886d
Rachit Jain <rachit.jain2095@gmail.com>
2021-09-08 23:43:52 +0530

    basic app

---
+++ script.js
@@ -0,0 +1,7 @@
+const startTrack = document.querySelector('button.startTrack');
+const stopTrack = document.querySelector('button.stopTrack');
+startTrack.addEventListener('click', () => {
+    // var cursorPosition = electron.screen.getCursorScreenPoint();
+    // console.log('x: ' + cursorPosition.x);
+    // console.log('y: ' + cursorPosition.y);
+});
